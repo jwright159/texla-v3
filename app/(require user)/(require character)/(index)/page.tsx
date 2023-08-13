dbbabffc5e9036a8c0b80b8613beac4dd4ee5fec
Jade Wright <iamjwright159@gmail.com>
2023-08-13 00:13:54 -0400

    Move current box elements to their own boxes

--- app/(require user)/(require character)/(index)/page.tsx
+++ app/(require user)/(require character)/(index)/page.tsx
@@ -6,16 +6,15 @@ import { useCharacter, usePlayerCharacter } from "@/lib/client/character"
 import { usePlayerRoom } from "@/lib/client/room"
 import { ReactElement, useEffect, useRef, useState } from "react"
 import styles from "./page.module.css"
+import { usePlayerUser } from "@/lib/client/user"
 
 export default function Index()
 {
+	const user = usePlayerUser()!
 	const character = usePlayerCharacter()!
 	const room = usePlayerRoom()
 
-	const [nodes, setNodes] = useState<ReactElement[]>([
-		<div key={0}>Welcome, {character.name}</div>,
-		<CharacterList key={1}/>
-	])
+	const [nodes, setNodes] = useState<ReactElement[]>([])
 	const [nodeCount, setNodeCount] = useState(nodes.length)
 
 	const [input, setInput] = useState("")
@@ -36,38 +35,44 @@ export default function Index()
 			<h1 className={styles.header}>You are in <span style={{fontFamily: "monospace"}}>{room?.name ?? "null"}</span></h1>
 
 			<div className={styles.game}>
-				<div className={styles.gameBox}>
-					<ul className={styles.nodes}>
-						{nodes.map(node => <li key={node.key} className={styles.node}>{node}</li>)}
-					</ul>
-					<div ref={scrollTo}></div>
-				</div>
+				<div className={styles.messages}>
+					<div className={[styles.gameBox, styles.bordered].join(" ")}>
+						<ul className={styles.nodes}>
+							{nodes.map(node => <li key={node.key} className={styles.node}>{node}</li>)}
+						</ul>
+						<div ref={scrollTo}></div>
+					</div>
 
-				<div className={styles.inputWrapper}>
-					<input
-						value={input}
-						onChange={event => setInput(event.target.value)}
-						onKeyDown={event =>
-						{
-							if (event.key === "Enter")
+					<div className={styles.inputWrapper}>
+						<input
+							value={input}
+							onChange={event => setInput(event.target.value)}
+							onKeyDown={event =>
 							{
-								event.preventDefault()
-								addTextToBox()
-							}
-						}}
-						className={styles.input}
-						placeholder="Input command..."
-					></input>
-					<button
-						className={styles.submitButton}
-						onClick={addTextToBox}
-					>→</button>
+								if (event.key === "Enter")
+								{
+									event.preventDefault()
+									addTextToBox()
+								}
+							}}
+							className={[styles.input, styles.bordered].join(" ")}
+							placeholder="Input command..."
+						></input>
+						<button
+							className={styles.submitButton}
+							onClick={addTextToBox}
+						>→</button>
+					</div>
+				</div>
+				
+				<div className={[styles.bordered, styles.characters].join(" ")}>
+					<CharacterList/>
 				</div>
 			</div>
 
 			<div>
-				<LogoutButton/>
-				<LogoutCharacterButton/>
+				Logged in as {user.username} &nbsp; <LogoutButton/> &nbsp;
+				Playing as {character.name} &nbsp; <LogoutCharacterButton/> &nbsp;
 			</div>
 		</main>
 	)
@@ -75,19 +80,13 @@ export default function Index()
 
 function CharacterList()
 {
-	const character = usePlayerCharacter()!
 	const room = usePlayerRoom()
-	const otherCharacters = room?.characterIds.filter(characterId => characterId !== character.id)
-
-	return (otherCharacters?.length ?
-		<div>
-			Also in room:
-			<ul>
-				{otherCharacters.map(characterId => <CharacterItem key={characterId} id={characterId}/>)}
-			</ul>
-		</div>
+	return (room ?
+		<ul>
+			{room.characterIds.map(characterId => <CharacterItem key={characterId} id={characterId}/>)}
+		</ul>
 	:
-		<div>No one else is here</div>
+		"Nobody came."
 	)
 }
 
